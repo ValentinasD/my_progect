a30860a04511db0e5390e39c25f25e250e64220f
valentinas daujotas <valentinas>
2023-05-29 17:33:42 +0300

    sukurta klase super user admin create new class NewSupplier

--- sandelys/admin.py
+++ sandelys/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import EnterNewProduct
 # Register your models here.
+from django.contrib import admin
+# Create your views here.
+from .models import EnterNewProduct
+
+admin.site.register(EnterNewProduct)
--- sandelys/models.py
+++ sandelys/models.py
@@ -9,25 +9,44 @@ from django.forms import ModelForm
 #         abstract = True
 
 
+
+
 CONDITION_VALUE = [('nau', 'nauja'),
-                   ('nuk', 'nukainuota'),
+                   ('nuk','nukainuota'),
                    ]
+
+MANUFACTURER_VALUE=[('AEG','AEG'),
+                    ('SIM', 'SIMENS'),
+                    ('BOS', 'BOSCH'),
+                    ('BEK', 'BEKO'),
+                    ]
+
+
+class NewSupplier(models.Model):
+        supplier_name = models.CharField(blank=False, null=False, max_length=50)
+        company_code = models.CharField(blank=False, null=False, max_length=30)
+        company_address_country = models.CharField(blank=False, null=False, max_length=30)
+        company_address_city = models.CharField(blank=False, null=False, max_length=30)
+        company_address_street = models.CharField(blank=False, null=False, max_length=30)
+        company_bank_account = models.CharField
+        company_vat_code = models.CharField
 
 
 class EnterNewProduct(models.Model):
     date_of_purchase = models.DateField(auto_now=True, blank=False, null=False)
     supplier_of_goods = models.CharField(blank=False, null=False, max_length=50)
     invoice_number = models.CharField(blank=False, null=False, max_length=15)
-    manufacturer = models.CharField(blank=False, null=False, max_length=20)
+    manufacturer = models.CharField(choices=MANUFACTURER_VALUE, blank=False, null=False, max_length=20)
     product_category = models.CharField(blank=False, null=False, max_length=20)
     product_model = models.CharField(blank=False, null=False, max_length=20)
     quantity = models.CharField(blank=False, null=False, max_length=4)
     condition = models.CharField(choices=CONDITION_VALUE, max_length=15)
     product_price = models.FloatField(blank=False, null=False, max_length=15)
 
-    def __str__(self):
+    def __repr__(self):
         return self.date_of_purchase, self.supplier_of_goods, self.invoice_number, self.manufacturer, \
-               self.product_category, self.product_model, self.quantity,self.condition, self.product_price
+               self.product_category, self.product_model, self.quantity, self.condition, self.product_price
+
 
 
 
